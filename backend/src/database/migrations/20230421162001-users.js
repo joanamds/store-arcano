03ea047bd7be028152529749086d758816d71c91
joanamds <joanamdsantos1@gmail.com>
2023-04-21 13:25:07 -0300

    feat: migration de users criada

---
+++ backend/src/database/migrations/20230421162001-users.js
@@ -0,0 +1,77 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      firstname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address_geolocation_lat: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address_geolocation_long: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address_city: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address_street: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address_number: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      address_zipcode: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  },
+};
