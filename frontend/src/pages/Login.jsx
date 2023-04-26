0dc7d671611c52e9319066c3309346164567dca1
joanamds <joanamdsantos1@gmail.com>
2023-04-26 12:41:54 -0300

    feat: adicionado formulário de login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = ({ target }) => {
+    if (target.name === 'email') {
+      setEmail(target.value);
+    } else {
+      setPassword(target.value)
+    }
+  }
+
   return (
     <form>
-      <input type="text" />
+      <label htmlFor="email">
+        <input
+          type="text"
+          name="email"
+          value={ email }
+          onChange={ handleChange }
+          placeholder='email'
+        />
+      </label>
+      <label htmlFor="password">
+        <input
+          type="password"
+          name="password"
+          value={ password }
+          onChange={ handleChange }
+          placeholder='password'
+        />
+      </label>
+      <button>Entrar</button>
     </form>
   );
 }
